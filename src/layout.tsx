974d4a068f59a5cd31cfe96e4cd4e087b01ece67
DearYuto <reasons@kakao.com>
2024-03-20 04:51:57 +0900

    feat: 리액트 토스티파이 라이브러리 적용

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,11 +1,19 @@
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 import HomePage from './pages/home';
+
 import '@/styles/main.css';
 
 function Layout() {
   return (
-    <section className="layout">
-      <HomePage />
-    </section>
+    <div className="layout">
+      <ToastContainer limit={5} autoClose={2_000} />
+      <section className="container">
+        <h1 className="title">TicTackTional</h1>
+        <HomePage />
+      </section>
+    </div>
   );
 }
 
