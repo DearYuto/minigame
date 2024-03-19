3178220f7ff7d48636467a804216ba504ca0db88
DearYuto <reasons@kakao.com>
2024-03-20 05:01:36 +0900

    feat: 플레이어 기본 값 정의, 타입 추가

---
+++ src/components/Player/constants/player.ts
@@ -0,0 +1,11 @@
+import { Player } from '../types/player';
+
+export const PLAYER1: Player = {
+  color: 'blue',
+  mark: 'circle',
+};
+
+export const PLAYER2: Player = {
+  color: 'red',
+  mark: 'square',
+};
---
+++ src/components/Player/types/player.ts
@@ -0,0 +1,4 @@
+export type Player = {
+  mark: 'circle' | 'x' | 'triangle' | 'square' | 'star';
+  color: 'red' | 'blue' | 'yellow' | 'green' | 'pink';
+};
