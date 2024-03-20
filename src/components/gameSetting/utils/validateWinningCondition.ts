3772257fd472137835145e6123a6d75595ae682f
DearYuto <reasons@kakao.com>
2024-03-20 09:05:25 +0900

    feat: validateWinningCondition 유틸 함수 추가

---
+++ src/components/gameSetting/utils/validateWinningCondition.ts
@@ -0,0 +1,7 @@
+import { GAME_RULE } from '@/constants/gameRule';
+
+export const validateWinningCondition = (condition: number, boardSize: number) => {
+  if (condition > boardSize || condition < GAME_RULE.boardSize) return false;
+
+  return true;
+};
