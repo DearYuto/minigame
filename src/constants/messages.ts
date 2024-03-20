8ea4dcb3ae7b9c8ce84cab9a9c5246638607428a
DearYuto <reasons@kakao.com>
2024-03-20 09:05:54 +0900

    feat: 상수 에러 메시지 추가

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -4,6 +4,7 @@ const ERROR_MESSAGE = {
   MIN_SIZE: `${GAME_RULE.boardSize} 이하로는 설정할 수 없어요.`,
   MAX_SIZE: `${GAME_RULE.maxBoardSize} 이상으로 설정할 수 없어요.`,
   NOT_A_NUMBER: '숫자 값만 입력할 수 있어요.',
+  MAX_CONDITION_SIZE: '게임 맵 사이즈 이상으로 설정할 수 없어요.',
 } as const;
 
 export { ERROR_MESSAGE };
