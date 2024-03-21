8778229e6ae2e86ac97c2838e0a4c075729acfbc
DearYuto <reasons@kakao.com>
2024-03-22 04:24:54 +0900

    feat: 게임 결과 컴포넌트 추가

---
+++ src/components/gameResult/index.tsx
@@ -0,0 +1,36 @@
+import { useContext } from 'react';
+
+import { Player } from '../player/types/player';
+
+import { GameActionsContext } from '@/store/contextAPI/GameProvider';
+
+type Props = {
+  winner: Player['id'] | undefined;
+  board: Array<Array<number>>;
+  history: number[][];
+};
+export default function GameResult({ winner, board, history }: Props) {
+  const dispatch = useContext(GameActionsContext);
+
+  const onClickSave = () => {
+    const gameResult = { winner, board, history };
+    localStorage.setItem('gameHistory', JSON.stringify(gameResult));
+  };
+
+  const onClickMoveToMain = () => {
+    dispatch({
+      type: 'CHANGE_STEP',
+      value: 'MAIN',
+    });
+  };
+
+  return (
+    <div>
+      <h2>게임 결과</h2>
+      <p>{!winner ? '무승부' : `플레이어 ${winner}님의 승리입니다.`}</p>
+
+      <button onClick={onClickSave}>게임 결과 저장</button>
+      <button onClick={onClickMoveToMain}>메인으로</button>
+    </div>
+  );
+}
