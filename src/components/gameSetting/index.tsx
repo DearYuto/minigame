e70a858c55f92e4eb094e91bb8d465782777af5a
DearYuto <reasons@kakao.com>
2024-03-21 23:07:23 +0900

    feat: 플레이어의 마크, 색상이 모두 동일한 경우 예외처리 로직 추가

--- src/components/gameSetting/index.tsx
+++ src/components/gameSetting/index.tsx
@@ -61,6 +61,12 @@ export default function GameSetting() {
   };
 
   const onClickStart = () => {
+    // TODO 플레이어 확장성 고려하면 하드코딩하면 안될듯 ?
+    if (players[0].mark === players[1].mark && players[0].color === players[1].color) {
+      toast.error('모든 플레이어의 마크와 색상이 동일합니다.');
+      return;
+    }
+
     dispatch({
       type: 'CHANGE_STEP',
       value: 'GAME',
