66170d472b310fed1f8228c4d709d23b10a41f34
DearYuto <reasons@kakao.com>
2024-03-21 18:27:54 +0900

    feat: 게임 시작, 기록 버튼 클릭 시 온클릭 이벤트 추가

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,8 +1,36 @@
+import { useContext } from 'react';
+
+import { GameActionsContext } from '@/store/contextAPI/GameProvider';
+
+import type { GameAction } from '@/store/contextAPI/types/gameState';
+
 export default function Main() {
+  const dispatch = useContext(GameActionsContext);
+
+  const onClick = (action: GameAction) => () => {
+    dispatch(action);
+  };
+
   return (
     <div className="buttons">
-      <button className="button button--start">게임 시작</button>
-      <button className="button button--record">기록</button>
+      <button
+        onClick={onClick({
+          type: 'CHANGE_STEP',
+          value: 'SETTING',
+        })}
+        className="button button--start"
+      >
+        게임 시작
+      </button>
+      <button
+        onClick={onClick({
+          type: 'CHANGE_STEP',
+          value: 'HISTORY',
+        })}
+        className="button button--record"
+      >
+        기록
+      </button>
     </div>
   );
 }
